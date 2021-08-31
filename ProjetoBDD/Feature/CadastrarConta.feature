963e494d7870fb98b2221cef72c1f07a0c9a74e1
Raphael Augusto Pereira de Assis <raphael.assis@totvs.com.br>
2021-08-31 11:38:45 -0300

    subindo projeto para github

---
+++ ProjetoBDD/Feature/CadastrarConta.feature
@@ -0,0 +1,20 @@
+﻿#language: pt
+#encoding: iso-8859-1
+
+Funcionalidade: Cadastrar conta
+	como um usuário do sistema
+	eu quero cadastrar contas de débito ou crédito
+	para que eu possa gerenciar contas
+
+Esquema do Cenário: Cadastro de conta com sucesso.
+	Dado Acessar a página de cadastro de conta
+	E Informar o nome da conta <nome>
+	E Informar o valor da conta <valor>
+	E Selecionar o tipo da conta <tipo>
+	Quando Solicitar a realização do cadastro da conta
+	Então Sistema informa que a conta foi cadastrada com sucesso
+
+Exemplos:
+	| nome           | valor  | tipo      |
+	| "Salário"      | "1000" | "Crédito" |
+	| "Conta de Luz" | "300"  | "Débito"  |
---
+++ ProjetoBDD/TestSteps/CadastrarContaTestSteps.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace ProjetoBDD.TestSteps
+{
+    [Binding]
+    public class CadastrarContaTestSteps
+    {
+        private IWebDriver _driver;
+
+        [Given(@"Acessar a página de cadastro de conta")]
+        public void DadoAcessarAPaginaDeCadastroDeConta()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+
+            _driver.Navigate().GoToUrl("http://localhost:13951/Contas/Cadastro");
+        }
+
+        [Given(@"Informar o nome da conta ""(.*)""")]
+        public void DadoInformarONomeDaConta(string nome)
+        {
+            var element = _driver.FindElement(By.XPath("//*[@id=\"nome\"]"));
+            element.Clear();
+            element.SendKeys(nome);
+        }
+
+        [Given(@"Informar o valor da conta ""(.*)""")]
+        public void DadoInformarOValorDaConta(int valor)
+        {
+            var element = _driver.FindElement(By.XPath("//*[@id=\"valor\"]"));
+            element.Clear();
+            element.SendKeys(valor.ToString());
+        }
+
+        [Given(@"Selecionar o tipo da conta ""(.*)""")]
+        public void DadoSelecionarOTipoDaConta(string tipo)
+        {
+            var element = _driver.FindElement(By.XPath("//*[@id=\"tipo\"]"));
+            var select = new SelectElement(element);
+
+            select.SelectByText(tipo);
+        }
+
+        [When(@"Solicitar a realização do cadastro da conta")]
+        public void QuandoSolicitarARealizacaoDoCadastroDaConta()
+        {
+            //gravando um screenshot do teste
+            var screenshot = ((ITakesScreenshot)_driver).GetScreenshot();
+            screenshot.SaveAsFile($"c:\\temp\\{Guid.NewGuid()}.png", ScreenshotImageFormat.Png);
+
+            var element = _driver.FindElement(By.XPath("//*[@id=\"btn-cadastro\"]"));
+            if (element.Displayed && element.Enabled)
+                element.Click();
+        }
+
+        [Then(@"Sistema informa que a conta foi cadastrada com sucesso")]
+        public void EntaoSistemaInformaQueAContaFoiCadastradaComSucesso()
+        {
+            var alert = _driver.SwitchTo().Alert();
+            Assert.AreEqual(alert.Text, "Dados enviados com sucesso!");
+
+            alert.Accept();
+
+            _driver.Close();
+            _driver.Quit();
+        }
+    }
+}
